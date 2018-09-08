faf0ea8da5d2fdad21801ec186bdd3768fbacbc3
查理大叔 <sharp.bruce.lee@gmail.com>
2018-09-08 16:27:55 +0800

    feat: add new parser (#6)
    * add configuration for typescript
    
    * add ts related
    
    * move config to src and types in definitation
    
    * add util
    
    * add unit test in jest
    
    * change ts config, fix undefined issue
    
    * add ignore options
    
    * add tool ramda & make related changes
    
    * fix vscode can not detect task problem
    
    * add test related framewords
    
    * add demo test with ts-jest and jest
    
    * test: 💍 Test Util.getCurrentDate method
    
    * Use another way to get pr list
    
    * feat: 🎸 add new pr parser
    
    * feat: 🎸 add new parser to deal with pull requests
    
    BREAKING CHANGE: Used new github api, add a parser which do parse
    
    * refactor: 💡 identify different kinds of pull requests
    
    BREAKING CHANGE: Add new parser may change category of pull requests

--- src/util.ts
+++ src/util.ts
@@ -1,11 +1,26 @@
-import moment from 'moment';
+import moment from "moment";
 
+const DEFAULT_DATE_FORMAT = "YYYY-MM-DD";
 export function sleep(ms: number) {
   return new Promise(resolve => {
     setTimeout(resolve, ms);
   });
 }
 
-export function getCurrentDate(): string {
-  return moment().format('YYYY-MM-DD');
+/**
+ * Format current date with formatter
+ * @param date   Date string formatted like this "20130208T080910".
+ * You can refer to https://momentjs.com/docs/#/parsing/ for more details.
+ * @param format YYYY-MM-DDTHH:MM:SSSZ or YYYY-MM-DD, or any
+ * kind formatter `moment` supports
+ */
+export function getCurrentDate(date?: string, formatter?: string): string {
+  if (!formatter) formatter = DEFAULT_DATE_FORMAT;
+  if (!date)
+    return moment()
+      .utc()
+      .format(formatter);
+  return moment(date)
+    .utc()
+    .format(formatter);
 }
