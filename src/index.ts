d2158861b89bf71ac0e0c074cb17091731c74e26
caffffe <1223840780@qq.com>
2018-11-30 16:50:29 +0800

    update account file

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { config } from "./config";
 import { env } from "./env";
 import * as Util from "./util";
 import { PassThrough } from "stream";
-import account from './account.js';
+import account from './account';
 
 /**
  * TODO:
